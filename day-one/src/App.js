c33342b906c0fc67f3759bcf64c77cd36616d785
yeexng <118996751+yeexng@users.noreply.github.com>
2023-01-30 16:27:13 +0000

    cleaner code

--- day-one/src/App.js
+++ day-one/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ButtonComponent from "./Components/ButtonComponent";
 import ImageComponent from "./Components/ImageComponent";
@@ -7,19 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ImageComponent link="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React Homework</p>
+
         <ButtonComponent btntext="This is a button" />
         <ButtonComponent btntext="This is another button" />
       </header>
